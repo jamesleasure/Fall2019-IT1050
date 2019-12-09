de1443435e16360ab862776dd5a4e0088c6742cf
Lab D 205 <labroom@tri-c.edu>
2019-12-09 10:20:33 -0500

    Added InClass07

---
+++ InClass07/InClass07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InClass07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Receipt receipt1 = new Receipt();
+
+            Console.WriteLine("New Receipt");
+
+            while(true)
+            {
+                Console.Write("Enter Description (e to end): ");
+                string description = Console.ReadLine();
+                if (description == "e")
+                {
+                    break;
+                }
+                Console.Write("Enter Amount: ");
+                double amount = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter Quantity: ");
+                int quantity =Convert.ToInt32(Console.ReadLine());
+                receipt1.AddItem(description, amount, quantity);
+            }
+            //receipt1.AddItem("Classic Italian Sub", 2.49, 1);
+            //receipt1.AddItem("Side of Fruit", 1.25, 2);
+            receipt1.PrintReceipt();
+            
+
+        }
+    }
+}
---
+++ InClass07/InClass07/ReceiptItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClass07
+{
+    class ReceiptItem
+    {
+        public string description;
+        public double amount;
+        public int quantity;
+
+        public ReceiptItem(string description, double amount, int quantity)
+        {
+            this.description = description;
+            this.amount = amount;
+            this.quantity = quantity;
+        }
+
+        public void Print()
+        {
+            Console.Write($"{this.quantity + "x", 3}");
+            Console.Write($"{this.description + "   ", 15}");
+            Console.Write($"{"@$" + this.amount, 5}");
+            Console.Write($"{"$" + (this.amount*this.quantity), 5}");
+        }
+    }
+}
